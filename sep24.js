a116b6bfbf325cba0c48140a66ec9da67a8486a6
Ditt namnFuna <haifa.bamehriz@chasacademyl..se>
2025-09-24 10:16:52 +0200

    tadding documents to practie

---
+++ sep24.js
@@ -0,0 +1,15 @@
+a. // MÅL: Räkna ut slutbetyget utifrån provresultat och inlämnade projekt.
+
+b. // INPUTS: Vilka variabler som kommer till funktionen och vilken typ de
+
+ positivt heltal.
+c. // RETURN: Vad ska skickas tillbaka efter beräkning eller behandling av
+
+inputs och vilken typ har den – tex. result: decimaltal.
+
+d. // STEG 1: Beskriv nu i tur och ordning (STEG 2, osv.) hur koden logiskt ska
+fungera för att lösa uppgiften - tänk som ett bakrecept – och gör det
+tydligt. Spara allt i GitHub, rotera driver och gå vidare med nästa uppgift!
+
+
+//
